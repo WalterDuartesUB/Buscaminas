cddadc22aa67065c3b5fcd745963562569ed81ab
WalterDuartesUB <walter.duartes@comunidad.ub.edu.ar>
2018-05-08 09:34:09 -0300

    Se agrega el enum de dificultad para determinar el porcentaje de minas del mapa

---
+++ Buscaminas/src/ar/edu/ub/testing/buscaminas/modelo/Dificultad.java
@@ -0,0 +1,11 @@
+package ar.edu.ub.testing.buscaminas.modelo;
+
+public enum Dificultad {
+	UN_JUGADOR_FACIL,
+	UN_JUGADOR_NORMAL,
+	UN_JUGADOR_DIFICIL,
+	SUPERVIVENCIA,
+	CONQUISTA,
+	CARRERA_RAPIDA,
+	CARRERA_LENTA;
+}
