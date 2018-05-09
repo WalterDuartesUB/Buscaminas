b461d8b89c121bb56d56a1c1c98838cb0294acab
Fernando Romero <fromero@HCJSM.local>
2018-05-09 12:20:43 -0300

    Clases modocarrera y conquista
    Se agrega los extends y los todo para resolver despues

--- Buscaminas/src/ar/edu/ub/testing/buscaminas/mododejuego/ModoDeJuegoCarrera.java
+++ Buscaminas/src/ar/edu/ub/testing/buscaminas/mododejuego/ModoDeJuegoCarrera.java
@@ -1,5 +1,20 @@
 package ar.edu.ub.testing.buscaminas.mododejuego;
 
-public class ModoDeJuegoCarrera{
-	// TODO extends ModoDeJuego
+import ar.edu.ub.testing.buscaminas.celda.ContenidoBomba;
+import ar.edu.ub.testing.buscaminas.modelo.ModoDeJuego;
+
+public class ModoDeJuegoCarrera extends ModoDeJuego{
+
+	@Override
+	public void actualizarEstadoJuego(ContenidoBomba bomba) {
+		
+		
+		//TODO Al encontrar una bomba, se termina el turno
+		this.setTerminoJuego(true);
+		
+		
+		//TODO No se encontro una bomba, sigue jugando
+		this.setTerminoJuego(false);
+		
+	}
 }
--- Buscaminas/src/ar/edu/ub/testing/buscaminas/mododejuego/ModoDeJuegoConquista.java
+++ Buscaminas/src/ar/edu/ub/testing/buscaminas/mododejuego/ModoDeJuegoConquista.java
@@ -1,5 +1,27 @@
 package ar.edu.ub.testing.buscaminas.mododejuego;
 
-public class ModoDeJuegoConquista {
-	// TODO extends ModoDeJuego
+import ar.edu.ub.testing.buscaminas.celda.ContenidoBomba;
+import ar.edu.ub.testing.buscaminas.modelo.ModoDeJuego;
+
+public class ModoDeJuegoConquista extends ModoDeJuego{
+
+	
+	public ModoDeJuegoConquista() {
+		super();
+	}
+	
+	@Override
+	public void actualizarEstadoJuego(ContenidoBomba bomba) {
+		
+		
+		
+		//TODO Encuentra una bomba, sigue jugando 
+		this.setTerminoJuego(false);
+		
+		
+		//TODO No encuentra bomba, se termina el turno
+		this.setTerminoJuego(true);
+		
+	}
+	
 }
